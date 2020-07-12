c6812b7d96bb5fc4fcf59fd9c84143b869362b99
villycivneva <villycivneva@gmail.com>
2020-07-12 23:59:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,31 +8,23 @@
 #include <fstream>
 
 int main() {
-    //Dinosaur dinosaur1("Rexa",Gender::Female,Era::Jurassic,"Tiranosavyr");
-    //Dinosaur dinosaur2("Rex",Gender::Male,Era::Jurassic,"Tiranosavyr");
-    //Dinosaur dinosaur3("Re",Gender::Female,Era::Jurassic,"Tiranosavyr");
+    Dinosaur dinosaur1("Rexa",Gender::Female,Era::Jurassic,"Tiranosavyr");
+    Dinosaur dinosaur2("Rex",Gender::Male,Era::Jurassic,"Tiranosavyr");
 
-    /*dinosaur1.storeInBinaryFile();
+    dinosaur1.storeInBinaryFile();
     dinosaur2.storeInBinaryFile();
-    dinosaur3.storeInBinaryFile();
-     */
 
-    class Carnivorous carnivorous("Hello1",Gender::Male,Era::Triassic,"pteradoktil");
+    class Carnivorous carnivorous("Carcho",Gender::Male,Era::Triassic,"pteradoktil");
     carnivorous.print();
-    //class Flying flying1("Hello2",Gender::Female,Era::Triassic,"kor",Food::Fish,Order::Carnivorous);
-    //class Flying flying2("Hello3",Gender::Male,Era::Triassic,"pter",Food::Fish,Order::Carnivorous);
+
+    Cage cage(Climate::Air,Era::Triassic,3);
 
 
+    cage.addDinosaurInTheCage(carnivorous);
+    cage.addDinosaurInTheCage(flying1);
 
-   // Cage cage(Climate::Air,Era::Triassic,3);
-
-
-    //cage.addDinosaurInTheCage(carnivorous);
-    //cage.addDinosaurInTheCage(flying1);
-    //cage.addDinosaurInTheCage(flying2);
-
-    //cage.storeInBinaryFile();
-    //cage.print();
+    cage.storeInBinaryFile();
+    cage.print();
 
     return 0;
 }
